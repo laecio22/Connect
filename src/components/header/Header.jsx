8dad85deabaf71abef7bf2f13deb158fb4b2a8bf
laecio22 <laecon22@hotmail.com>
2026-01-30 10:36:39 -0300

    style(header.module.scss):ajuste  estilização  header  e efeito  de  toggle  no dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,42 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
-import PhotoPerfil from '../../assets/images/laecio.jpg';
-import S from './header.module.scss';
+import PhotoPerfil from "../../assets/images/laecio.jpg";
+import S from "./header.module.scss";
+import { useState } from "react";
 
 const Header = () => {
+  const [menuAberto, setMenuAberto] = useState(false)
   return (
     <>
       <header className={S.header}>
         <section>
           <Link to="/">
             <img
+              className={S.imgLogo}
               src={Logo}
               alt="logo da aplicação representado por uma  imagem de uma  mão segurando um coração"
             />
           </Link>
-        </section>       
+        </section>
         <nav className={S.nav}>
-          <ul>
-            <Link to="/doacao">Doação</Link>
-            <Link to="/voluntariado">Voluntariado</Link>
-            <Link to="/mentoria">Mentorias</Link>
-            <Link to="/eventos">Eventos</Link>
-          </ul>
+          <Link to="/doacao">Doação</Link>
+          <Link to="/voluntariado">Voluntariado</Link>
+          <Link to="/mentoria">Mentorias</Link>
+          <Link to="/eventos">Eventos</Link>
         </nav>
-        <img src={PhotoPerfil} alt="Minha foto  do perfil de usuário" />
+        <img
+          src={PhotoPerfil}
+          alt="Minha foto  do perfil de usuário"
+          className={S.imgUser}
+          onClick={()=>setMenuAberto(!menuAberto)}
+        />
       </header>
-      <Link to="/">Laécio da Conceição</Link>
+      <nav className={menuAberto ? S.navUser: S.closedNav}>
+        <Link to="/usuario">Laécio da Conceição</Link>
+        <Link to="/">Meu Voluntariado</Link>
+        <Link to="/">Configurações  de Conta</Link>
+        <Link to="/">Sair</Link>
+      </nav>
     </>
   );
 };
