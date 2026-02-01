fef33a702d74ee554fbae628f186707a526c3c73
laecio22 <laecon22@hotmail.com>
2026-02-01 13:07:36 -0300

    feat:(Home.jsx): Estruturação  do  html  da  página  Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,36 @@
-
+import ImagemPrincipal from "../../assets/images/imgPrincipal.png";
 const Home = () => {
   return (
-    <div>
-      <h1>Página Principal</h1>
-    </div>
+    <section>
+      <div>
+        <h1>Projetos Sociais que transformam</h1>
+        <div>
+          Conectamos sua empresa com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade.
+        </div>
+        <button>Cadastrar Empresa</button>
+        <div>
+          <article>
+            <h3>500+</h3>
+            <p>Empresas Voluntárias</p>
+          </article>
+          <article>
+            <h3>1.2k+</h3>
+            <p>Empresas Voluntárias</p>
+          </article>
+          <article>
+            <h3>50k+</h3>
+            <p>Vidas Impactadas</p>
+          </article>
+        </div>
+      </div>
+      <div>
+        <img
+          src={ImagemPrincipal}
+          alt="Imagem mãos  segurando  bonecos dando  as  mãos ao redor de um globo terrestre"
+        />
+      </div>
+    </section>
   );
 };
 
