67afcbda961b15df8716e666816e3060d101af0b
laecio22 <laecon22@hotmail.com>
2026-02-03 20:25:14 -0300

    style(cardInfo.module.scss):Estilização  do  componente  de  Card de estatísticas

--- src/components/cardiInfo/CardInfo.jsx
+++ src/components/cardiInfo/CardInfo.jsx
@@ -1,8 +1,8 @@
-
+import S from './cardInfo.module.scss';
 
 const CardInfo = ({title, text}) => {
   return (
-    <article>
+    <article className={S.article}>
        <h3>{title}</h3>
        <p>{text}</p>
     </article>
