7a02b4cb8defca7c7f693e93d11f6a486c5d699e
laecio22 <laecon22@hotmail.com>
2026-02-06 18:48:15 -0300

    bugfix(Home):ajuste  erro  ao subir Vercel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import ImagemPrincipal from "../../assets/images/imgPrincipal.png";
-import Button  from '../../components/button/Button';
+import Button from "../../components/button/Button";
 import CardInfo from "../../components/cardiInfo/CardInfo";
-import S from './home.module.scss';
+import S from "./home.module.scss";
 const Home = () => {
   return (
     <main className={S.main}>
@@ -11,18 +11,20 @@ const Home = () => {
           Conectamos sua empresa com projetos sociais impactantes. Juntos,
           criamos mudanças reais na comunidade.
         </div>
-        <Button isButtonSecondary hasIcon>Cadastrar Empresa</Button>   
-       
+        <Button isButtonSecondary hasIcon>
+          Cadastrar Empresa
+        </Button>
+
         <section className={S.infoDados}>
-          <CardInfo  title="500+" text="Empresas Voluntárias"/>
-          <CardInfo title="1.2K+" text="Projetos Realizados"/>
-          <CardInfo title="50K+" text="Vidas Impactadas"/>     
+          <CardInfo title="500+" text="Empresas Voluntárias" />
+          <CardInfo title="1.2K+" text="Projetos Realizados" />
+          <CardInfo title="50K+" text="Vidas Impactadas" />
         </section>
       </section>
-        <img
-          src={ImagemPrincipal}
-          alt="Imagem mãos  segurando  bonecos dando  as  mãos ao redor de um globo terrestre"
-        />   
+      <img
+        src={ImagemPrincipal}
+        alt="Imagem mãos  segurando  bonecos dando  as  mãos ao redor de um globo terrestre"
+      />
     </main>
   );
 };
