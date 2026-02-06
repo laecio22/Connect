70c91a0b0dd9f02eff1b40a05d5c0a56a55ba836
laecio22 <laecon22@hotmail.com>
2026-02-06 19:16:13 -0300

    style(Footer):estilização  do  componente footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import S from "./footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={S.footer}>
       © 2025 Vai na Web & Empower. Todos os direitos reservados. Projeto
       destinado exclusivamente a fins educativos.
     </footer>
